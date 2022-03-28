454bb8f4056bb2c1ca2bc87e203ce236309a6c38
mnyDEV <>
2022-03-28 01:46:48 +0000

    added more code :)

--- README.md
+++
@@ -1,1 +0,0 @@
-# mny-site
--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+// function will be added
 function myFunction() {
     var element = document.body;
     element.classList.toggle("dark-mode") }
+//same as line #1
+function myDark() {
+    var element = document.body;
+    element.classList.toggle("dark-mode") }
+//current functions in site
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+function twitterH() {
+		var snd = new Audio("Twitter-S.mp3"); snd.play();
+}
+
+`use strict`;
+function refreshTime() {
+  const timeDisplay = document.getElementById("time");
+  const dateString = new Date().toLocaleString();
+  const formattedString = dateString.replace(", ", " - ");
+  timeDisplay.textContent = formattedString;
+}
+  setInterval(refreshTime, 1000);
